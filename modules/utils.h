c7185bb49036e858ea915b93466b19a749dd56de
GustavoFantato <gugafantato@gmail.com>
2026-04-07 18:00:41 -0300

    Undo

--- modules/utils.h
+++ modules/utils.h
@@ -14,6 +14,8 @@ void binarioNaTela(char *arquivo);
 
 char *readCSVLine(char line[], int line_size, FILE *csvFile);
 
+int verifyIfNullField(char *field);
+
 void switchDataRecord(DataRecord *data, int i, char *field);
 
 void verifyIfDiffStation(char *name, char ***diffStationNames, int *stationsQtd);
